06876fb6ef30629b9c26c550f7ba42a0c4d9a3d0
Anton Vlasov <nanostet@gmail.com>
2021-02-21 11:25:56 +0700

    Remove rxjs links

--- packages/mobx-fog-of-war/src/index.tsx
+++ packages/mobx-fog-of-war/src/index.tsx
@@ -1,7 +1,5 @@
 export * from './argsToKey';
 export * from './Store';
-export * from './rxBatch';
-export * from './rxRequest';
 export * from './asyncRequest';
 export * from './sortByArgsArray';
 export * from './provideStores';
